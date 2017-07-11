2530a5cddfee3ce16d62283a84968db8921a8820
Iain Bryson <iain@iain-bryson.ca>
2017-07-11 13:02:33 -0700

    Add CORS headers so client/server can talk to one another.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,29 @@ class ApplicationController < ActionController::Base
 #  protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
 
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  # TODO: make this less open when we actually go to deploy
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
 end
