76fe58f43237ce7f3b45ed6223c3ecbd44d31d22
Iain Bryson <iain@iain-bryson.ca>
2017-11-03 17:27:55 -0700

    Minor fixes.

--- app/controllers/chaos_api/v6/question_controller.rb
+++ app/controllers/chaos_api/v6/question_controller.rb
@@ -12,22 +12,27 @@ module ChaosApi::V6
       @study_definition = StudyDefinition.find(params[:id])
       experiment_id = @study_definition.data
 
+      @response = ChaosExperimentService.new(@study_definition).make_slide(trial_index, @chaos_session.protocol_user_id)
+
+      # compare to version generated from experiment xml, if it exists
       ExperimentXmls.instance.refresh
       @experiment = ExperimentXmls.instance.experiment_by_id[experiment_id] || {}
       @experiment_n = ExperimentXmls.instance.experiment_n_by_id[experiment_id]
-      @results = ExperimentXmls.get_questions(@experiment_n, trial_index)
-      @response = ChaosResponse.new(@results)
-      @response.Body["FoundCount"] = @experiment_n.css("Experiment>Trials").children.count
-      @response.Body["StartIndex"] = trial_index
+      if @experiment != nil and @experiment_n != nil then
+        @results = ExperimentXmls.get_questions(@experiment_n, trial_index)
+        @exml_response = ChaosResponse.new(@results)
+        @exml_response.Body["FoundCount"] = @experiment_n.css("Experiment>Trials").children.count
+        @exml_response.Body["StartIndex"] = trial_index
 
-      @response = ChaosExperimentService.new(@study_definition).make_slide(trial_index, @chaos_session.protocol_user_id)
+        @response = ChaosExperimentService.new(@study_definition).make_slide(trial_index, @chaos_session.protocol_user_id)
 
-      Rails.logger.info("XML response #{@results.count} SD response #{@response.Body[:Results].count}")
-      # compare the prototype generation with the new created one
-      @results.each_with_index do |r, i| 
-#        ap i
-        core_model = @response.Body[:Results][i]
-#        ap core_model.deep_diff(r)
+        Rails.logger.info("XML response #{@results.count} SD response #{@response.Body[:Results].count}")
+        # compare the prototype generation with the new created one
+        @results.each_with_index do |r, i| 
+  #        ap i
+          core_model = @response.Body[:Results][i]
+  #        ap core_model.deep_diff(r)
+        end
       end
 
       respond_to do |format|
