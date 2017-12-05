e2894e631cd231abe70e2b9dfa25f12a935c7165
Iain Bryson <iain@iain-bryson.ca>
2017-12-05 13:22:49 -0600

    Make it clearer what parts of the front page and profile pages are yet to be implemented.

--- app/javascript/packs/components/front_page/FrontPage.jsx
+++ app/javascript/packs/components/front_page/FrontPage.jsx
@@ -1,5 +1,11 @@
-import React, { Component } from 'react';
+// Import React and Dependencies
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import {Link} from 'react-router-dom'
+
+// Import components
 import FrontPagePreface from './FrontPagePreface.html';
+
 var htmlDoc = {__html: FrontPagePreface};
 
 export default class FrontPage extends Component {
@@ -10,11 +16,12 @@ export default class FrontPage extends Component {
     return (
         <div id="wrap" className="admin-app-container container">
           <div className="row">
-            <div className="col-offset-xs-1 col-xs-11" dangerouslySetInnerHTML={htmlDoc} />
+            <div className="col-xs-offset-1 col-xs-11" dangerouslySetInnerHTML={htmlDoc} />
           </div>
           <div className="row">
-            <div className="col-offset-xs-1 col-xs-11">
+            <div className="col-xs-offset-1 col-xs-11">
               <h2>Studies you can take...</h2>
+              <p> In the future, you'll see a list here.  Until then <Link to="/participant">click here</Link> to see the studies you're eligeable for </p>
             </div>
           </div>
         </div>
--- app/javascript/packs/components/profile_page/ProfilePage.jsx
+++ app/javascript/packs/components/profile_page/ProfilePage.jsx
@@ -8,8 +8,22 @@ export default class ProfilePage extends Component {
     return (
         <div id="wrap" className="page-container container">
           <div className="row">
-            <div className="col-offset-xs-1 col-xs-11">
+            <div className="col-xs-offset-1 col-xs-11">
               <h2>Profile for {this.props.current_user.email}</h2>
+
+              <p>Role: <span><b>{this.props.current_user.role}</b></span></p>
+
+              <h3>Account</h3>
+
+              <div className='row'>
+                <button className='btn btn-primary col-xs-offset-1 col-xs-3' disabled={true}>Reset Password</button>
+                <button className='btn btn-danger col-xs-offset-1 col-xs-3' disabled={true}>Delete Account</button>
+              </div>
+
+              <h3>Study Participation</h3>
+
+              <p>Under Construction...</p>
+
             </div>
           </div>
         </div>
