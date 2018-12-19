384aa47543d62589c191474ebe4785f086ae3b29
Iain J. Bryson <you@example.com>
2018-12-19 19:58:51 +0700

    Log DataPoints filter query

--- app/controllers/api/v1/data_points_controller.rb
+++ app/controllers/api/v1/data_points_controller.rb
@@ -9,14 +9,20 @@ module Api::V1
       plural_resource_name = "@#{resource_name.pluralize}"
 
       pparams = params.permit([:study_id, :protocol_user_id, :phase_definition_id, :trial_definition_id, :component_id])
-      where_components = pparams.to_h.keys.select {|p| (p.to_s.end_with?('_id') && !params[p].nil?)}.map {|p| {p.to_sym => params[p]}}
+      filter_fields = pparams
+                          .to_h
+                          .keys
+                          .select{ |p| (p.to_s.end_with?('_id') && !params[p].nil?) }
+                          .map{ |p| {p.to_sym => params[p].to_i} }
+                          .reduce(&:merge)
 
-      where = where_components.reduce(&:merge)
+      Rails.logger.info "DataPoint where: #{filter_fields.ai}"
 
-      resources = StudyResult::DataPoint.where(where)
+      resources = StudyResult::DataPoint.where(filter_fields)
 
       if not page_params.nil?
-        resources = resources.page(page_params[:page])
+        resources = resources
+                        .page(page_params[:page])
                         .per(page_params[:page_size])
       end
       instance_variable_set(plural_resource_name, resources)
