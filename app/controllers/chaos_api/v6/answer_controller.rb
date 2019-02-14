7de47f1059d971f9e68a9d8a5594d7da96f65b21
Iain J. Bryson <you@example.com>
2019-02-13 16:42:35 -0800

    Fix lost stage issues

--- app/controllers/chaos_api/v6/answer_controller.rb
+++ app/controllers/chaos_api/v6/answer_controller.rb
@@ -52,11 +52,14 @@ module ChaosApi::V6
       end
 
       StudyResult::DataPoint.transaction do
-        # because CHAOS' semantics are to republish everything, blow away existing datapoints
+        # because CHAOS' semantics are to republish everything, blow away existing data points.
         # We can't easily go incremental because of chaos' rate limiting, which means some updates
         # might not fire, and it's not easy to keep track of which ones made it to the server and which
-        # ones didn't
-        StudyResult::DataPoint.where(datapoint_query_fields).delete_all
+        # ones didn't.
+        # Note that because state entities are updated separately, we don't nuke those.
+        StudyResult::DataPoint.where(datapoint_query_fields)
+            .where.not({:point_type => 'State'})
+            .delete_all
         new_datapoints.each(&:save!)
       end
 
