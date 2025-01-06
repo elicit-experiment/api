8c717e848fd7536036ea29ca23fe1d752460e210
iain Bryson <bryson.iain@gmail.com>
2025-01-06 05:07:29 -1000

    Finalize on the end of experiment.

--- app/models/study_result/experiment.rb
+++ app/models/study_result/experiment.rb
@@ -11,6 +11,14 @@ module StudyResult
     belongs_to :study_result, class_name: 'StudyResult::StudyResult'
 
     has_many :stages, class_name: 'StudyResult::Stage', dependent: :destroy
+    has_many :time_series, class_name: 'StudyResult::TimeSeries', through: :stages, dependent: :nullify # dependent of stage, which handles destroy
     has_many :trial_results, class_name: 'StudyResult::TrialResult', dependent: :destroy
+
+    def finalize(asynchronous: false, force: true)
+      raise 'Not implemented' if asynchronous
+
+      logger.info "Finalizing experiment #{id}; Time series: #{time_series.map(&:id).join(', ')}"
+      time_series.each { |series| series.finalize(force: force) }
+    end
   end
 end
