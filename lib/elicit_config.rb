f0a7b6afd602ab646dfbc792103c6fc96224b683
iain Bryson <bryson.iain@gmail.com>
2025-01-19 02:19:49 -1000

    Fix fragment setting in forming participant urls.

--- lib/elicit_config.rb
+++ lib/elicit_config.rb
@@ -9,9 +9,9 @@ class ElicitConfig
   end
 
   def participant_url(session_guid: nil, protocol_definition_id: nil)
-    URI("#{@participant_frontend[:scheme]}://#{@participant_frontend[:host]}:#{@participant_frontend[:port]}").tap do |base_uri|
+    Addressable::URI.parse("#{@participant_frontend[:scheme]}://#{@participant_frontend[:host]}:#{@participant_frontend[:port]}").tap do |base_uri|
       base_uri.query = "session_guid=#{session_guid}" if session_guid
-      base_uri.hash = "Experiment/#{protocol_definition_id}" if protocol_definition_id
+      base_uri.fragment = "Experiment/#{protocol_definition_id}" if protocol_definition_id
     end
   end
 
