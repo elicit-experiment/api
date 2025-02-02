28fe8ecd7a6c1507e70d472ebf3183d9268308bb
iainbryson <bryson.iain@gmail.com>
2025-02-02 13:04:21 -1000

    Allow environment overrides in Puma.

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,12 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+
+require 'dotenv'
+Dotenv.load
+
+max_threads_count = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))
+min_threads_count = Integer(ENV.fetch('RAILS_MIN_THREADS', max_threads_count))
 threads min_threads_count, max_threads_count
 
 # You can either set the env var, or check for development
@@ -16,18 +20,18 @@ end
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+port Integer(ENV.fetch('PORT', 3000))
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch('RAILS_ENV', 'development')
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -35,7 +39,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-web_concurrency = ENV.fetch("WEB_CONCURRENCY") { 1 }
+web_concurrency = Integer(ENV.fetch('WEB_CONCURRENCY', 1))
 workers web_concurrency if web_concurrency > 1
 
 # Use the `preload_app!` method when specifying a `workers` number.
