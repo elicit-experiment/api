604fb26e15c14e180a7bb609c8bdf37f732f55e1
iainbryson <bryson.iain@gmail.com>
2025-02-09 09:55:14 -0800

    Fix job storage

--- app/jobs/finalize_time_series_job.rb
+++ app/jobs/finalize_time_series_job.rb
@@ -1,7 +1,8 @@
 class FinalizeTimeSeriesJob < ApplicationJob
   queue_as :default
 
-  # FinalizeTimeSeriesJob.perform_later StudyResult::TimeSeries.has_no_finalized_file.first.stage.experiment.id
+  # FinalizeTimeSeriesJob.perform_later experiment_ids
+  # experiment_ids = StudyResult::TimeSeries.has_no_finalized_file.where.not(file: nil).take(10).map {|ts| ts.stage.experiment.id}
   def perform(*experiment_ids)
     StudyResult::Experiment.where(id: experiment_ids).find_each do |experiment|
       logger.info "Finalizing experiment #{experiment.id}"
--- bin/run-jobs-role.sh
+++ bin/run-jobs-role.sh
@@ -3,4 +3,7 @@
 : "${RAILS_ENV:=production}"
 export RAILS_ENV
 
+rm -rf /var/www/app/public
+ln -s /var/www/app/public/ public
+
 bundle exec bin/jobs
