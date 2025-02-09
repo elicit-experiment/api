a85f794bda84c22c0e2a1f349e1bc714b658b052
iainbryson <bryson.iain@gmail.com>
2025-02-09 10:11:50 -0800

    Fix job storage

--- bin/run-jobs-role.sh
+++ bin/run-jobs-role.sh
@@ -3,7 +3,9 @@
 : "${RAILS_ENV:=production}"
 export RAILS_ENV
 
-rm -rf /var/www/app/public
+mv public public_init
+rm -rf /var/www/app/public/assets
+cp -R public_init/* /var/www/app/public/
 ln -s /var/www/app/public/ public
 
 bundle exec bin/jobs
