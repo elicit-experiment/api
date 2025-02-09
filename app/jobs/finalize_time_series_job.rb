e9a721151b7a6771cd4e50f47fbabaad5ad46451
iainbryson <bryson.iain@gmail.com>
2025-02-09 10:40:35 -0800

    Add method to finalize old time series.

--- app/jobs/finalize_time_series_job.rb
+++ app/jobs/finalize_time_series_job.rb
@@ -1,6 +1,24 @@
 class FinalizeTimeSeriesJob < ApplicationJob
   queue_as :default
 
+  class << self
+    def enqueue_in_progress
+      in_progress = StudyResult::TimeSeries.has_no_finalized_file.where.not(file: nil)
+
+      experiment_ids = in_progress.take(100).map do |ts|
+        ts.stage.experiment.id
+      end
+
+      in_progress_active_storage = StudyResult::TimeSeries.has_no_finalized_file.where(file: nil).has_in_progress_file
+
+      experiment_ids << in_progress_active_storage.take(100).map do |ts|
+        ts.stage.experiment.id
+      end
+
+      FinalizeTimeSeriesJob.perform_later experiment_ids
+    end
+  end
+
   # FinalizeTimeSeriesJob.perform_later experiment_ids
   # experiment_ids = StudyResult::TimeSeries.has_no_finalized_file.where.not(file: nil).take(10).map {|ts| ts.stage.experiment.id}
   def perform(*experiment_ids)
