f45a0fc76a2edf21a94eae6a69e77aea8d482be9
iainbryson <bryson.iain@gmail.com>
2025-02-24 07:53:27 -0800

    Move FormattedDate to functional react component.SimpleModal

--- app/javascript/components/ui_elements/SimpleModal.jsx
+++ app/javascript/components/ui_elements/SimpleModal.jsx
@@ -3,31 +3,23 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import PropTypes from "prop-types";
 
-export class SimpleModal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Modal show={this.props.show}>
-        <Modal.Header>
-          <h4>{this.props.title}</h4>
-        </Modal.Header>
-        <Modal.Body>
-          <p>{this.props.subtitle}</p>
-          <hr/>
-          <p>{this.props.body}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.props.handleHideModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-}
+const SimpleModal = ({show, title, subtitle, body, handleHideModal}) => (
+  <Modal show={show}>
+    <Modal.Header>
+      <h4>{title}</h4>
+    </Modal.Header>
+    <Modal.Body>
+      <p>{subtitle}</p>
+      <hr/>
+      <p>{body}</p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleHideModal}>
+        Close
+      </Button>
+    </Modal.Footer>
+  </Modal>
+);
 
 SimpleModal.propTypes = {
   body: PropTypes.string.isRequired,
@@ -36,12 +28,5 @@ SimpleModal.propTypes = {
   subtitle: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 }
-/*
-SimpleModal.propTypes = PropTypes.shape({
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired,
-  show: PropTypes.string.isRequired,
-  handleHideModal: PropTypes.func.isRequired,
-}).isRequired;
-*/
+
+export default SimpleModal;
