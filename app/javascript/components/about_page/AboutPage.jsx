b382ac8461c11f723e1278bc1a339e2317081914
iainbryson <bryson.iain@gmail.com>
2025-02-24 11:45:35 -0800

    Move AboutPage to functional component.

--- app/javascript/components/about_page/AboutPage.jsx
+++ app/javascript/components/about_page/AboutPage.jsx
@@ -1,22 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Page from './About.html';
-var htmlDoc = {__html: Page};
+const htmlDoc = {__html: Page};
 
-export default class AboutPage extends Component {
-  constructor(props) {
-    super(props);
-  }
+const AboutPage  = () => (
+  <div id="wrap" className="page-container container">
+    <div className="row">
+      <div className="offset-xs-1 col-11">
+        <div dangerouslySetInnerHTML={htmlDoc}/>
+      </div>
+    </div>
+  </div>
+);
 
-  render() {
-    return (
-
-        <div id="wrap" className="page-container container">
-          <div className="row">
-            <div className="offset-xs-1 col-11">
-              <div dangerouslySetInnerHTML={htmlDoc}/>
-            </div>
-          </div>
-        </div>
-  )
-  }
-}
+export default AboutPage;
