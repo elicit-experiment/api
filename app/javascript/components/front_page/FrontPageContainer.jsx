f1ba2b729d8aad1facc8449d83bccb6aeff68599
iainbryson <bryson.iain@gmail.com>
2025-02-24 10:36:27 -0800

    Move Front Page to functional react.

--- app/javascript/components/front_page/FrontPageContainer.jsx
+++ app/javascript/components/front_page/FrontPageContainer.jsx
@@ -1,57 +1,11 @@
 import React from 'react'
 import FrontPage from "./FrontPage.jsx"
-import {currentUser} from "../../reducers/selector";
-import connect from "react-redux/es/connect/connect";
-import elicitApi from "../../api/elicit-api";
-import PropTypes from "prop-types";
-import {AnonymousProtocolsType, CurrentUserType} from "types";
+import {CurrentUserProvider} from "../../contexts/CurrentUserContext";
 
-class FrontPageContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        //props.syncCurrentUser();
-        if (!props.anonymous_protocols || (!props.anonymous_protocols.sync && !props.anonymous_protocols.loading)) {
-            props.loadAnonymousProtocols();
-        }
-    }
+const FrontPageContainer = () => (
+  <CurrentUserProvider>
+      <FrontPage/>
+  </CurrentUserProvider>
+)
 
-    render() {
-        return (
-                <FrontPage current_user_role={this.props.current_user.data.role}
-                           current_username={this.props.current_user.data.username}
-                           current_user_email={this.props.current_user.data.email}
-                           anonymous_protocols={this.props.anonymous_protocols}
-                           loadAnonymousProtocols={this.props.loadAnonymousProtocols}
-                />
-        )
-    }
-}
-
-FrontPageContainer.propTypes = {
-    syncCurrentUser: PropTypes.func,
-    loadAnonymousProtocols: PropTypes.func,
-    anonymous_protocols: AnonymousProtocolsType,
-    current_user: CurrentUserType,
-};
-
-FrontPageContainer.defaultProps = {
-    syncCurrentUser: () => {
-    },
-    loadAnonymousProtocols: () => {
-    },
-};
-
-const mapStateToProps = (state) => ({
-    anonymous_protocols: state.anonymous_protocols,
-    current_user: currentUser(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-    syncCurrentUser: () => dispatch(elicitApi.actions.current_user.sync()),
-    loadAnonymousProtocols: () => dispatch(elicitApi.actions.anonymous_protocols({public: true})),
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(FrontPageContainer);
+export default FrontPageContainer;
