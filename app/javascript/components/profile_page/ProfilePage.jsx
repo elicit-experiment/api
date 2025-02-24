ba81cd59211d11ed9c4d5b14d733b87a76ec520d
iainbryson <bryson.iain@gmail.com>
2025-02-24 10:20:07 -0800

    Move Profile Page to functional react.

--- app/javascript/components/profile_page/ProfilePage.jsx
+++ app/javascript/components/profile_page/ProfilePage.jsx
@@ -1,59 +1,57 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import {useCurrentUser} from "../../contexts/CurrentUserContext";
+import {UserType} from "../../types";
 
 const publicClientId = process.env.PUBLIC_CLIENT_ID;
 const publicClientSecret = process.env.PUBLIC_CLIENT_SECRET;
 
-export default class ProfilePage extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const apiSection = (this.props.current_user.role === 'investigator' || this.props.current_user.role === 'admin') ?
-      <section className="card p-2 mb-2">
-        <h3>API</h3>
-        <div className="offset-3"><span>Client ID: </span><pre>{publicClientId}</pre></div>
-        <div className="offset-3"><span>Client SECRET: </span><pre>{publicClientSecret}</pre></div>
-      </section>
-      : '';
+const ProfilePage = ({user}) => {
+  const currentUser = useCurrentUser();
+  const renderUser = user || currentUser;
 
-    return (
-        <div id="wrap" className="page-container container">
-          <div className="row">
-            <div className="offset-1 col-11">
-              <h2 className="mt-3">Profile for {this.props.current_user.email}</h2>
+  const apiSection = (currentUser.data.role === 'investigator' || currentUser.data.role === 'admin') ?
+    <section className="card p-2 mb-2">
+      <h3>API</h3>
+      <div className="offset-3"><span>Client ID: </span><pre>{publicClientId}</pre></div>
+      <div className="offset-3"><span>Client SECRET: </span><pre>{publicClientSecret}</pre></div>
+    </section>
+    : '';
 
-              <section className="card p-2 mb-2">
-                <p>Role: <span><b>{this.props.current_user.role}</b></span></p>
-              </section>
+  return (
+    <div id="wrap" className="page-container container">
+      <div className="row">
+        <div className="offset-1 col-11">
+          <h2 className="mt-3">Profile for {renderUser.data.email}</h2>
 
-              <section className="card p-2 mb-2">
-                <h3>Account</h3>
+          <section className="card p-2 mb-2">
+            <p>Role: <span><b>{renderUser.data.role}</b></span></p>
+          </section>
 
-                <div className='row'>
-                  <button className='btn btn-primary offset-3 col-3' disabled={true}>Reset Password</button>
-                  <button className='btn btn-danger ml-1 col-3' disabled={true}>Delete Account</button>
-                </div>
-                <p></p>
-              </section>
+          <section className="card p-2 mb-2">
+            <h3>Account</h3>
 
-              {apiSection}
-
-              <section className="card p-2 mb-2">
-                <h3>Study Participation</h3>
-                <p className="offset-3">Under Construction...</p>
-              </section>
-
+            <div className='row'>
+              <button className='btn btn-primary offset-3 col-3' disabled={true}>Reset Password</button>
+              <button className='btn btn-danger ml-1 col-3' disabled={true}>Delete Account</button>
             </div>
-          </div>
+            <p></p>
+          </section>
+
+          {apiSection}
+
+          <section className="card p-2 mb-2">
+            <h3>Study Participation</h3>
+            <p className="offset-3">Under Construction...</p>
+          </section>
+
         </div>
-    )
-  }
+      </div>
+    </div>
+  )
 }
 
 ProfilePage.propTypes = {
-  current_user: PropTypes.object.isRequired,
+  user: UserType,
 };
 
-ProfilePage.defaultProps = {
-};
+export default ProfilePage;
