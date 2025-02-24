62e4b8732a90abad4deb8b6318ce4238fe9f32f9
iainbryson <bryson.iain@gmail.com>
2025-02-24 07:53:13 -0800

    Move FormattedDate to functional react component.

--- app/javascript/components/ui_elements/FormattedDate.jsx
+++ app/javascript/components/ui_elements/FormattedDate.jsx
@@ -2,17 +2,11 @@ import PropTypes from 'prop-types'
 import React from 'react';
 const dateFormat = require('dateformat');
 
-export default class ParticipantProtocol extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render() {
-    const date = Date.parse(this.props.date);
-    const dateText = dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-    return (<span>{dateText}</span>)
-  }
-}
+const ParticipantProtocol = ({date}) => {
+  const parsedDate = Date.parse(date);
+  const dateText = dateFormat(parsedDate, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+  return (<span>{dateText}</span>)
+};
 
 ParticipantProtocol.propTypes = {
   date: PropTypes.object.isRequired,
