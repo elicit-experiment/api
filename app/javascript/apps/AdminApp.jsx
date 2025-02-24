53f67d927114afee7ebbd47e9c4e4cd21ea811da
iainbryson <bryson.iain@gmail.com>
2025-02-24 08:27:09 -0800

    Refactor to move all current setting logic to ensureCurrentUser

--- app/javascript/apps/AdminApp.jsx
+++ app/javascript/apps/AdminApp.jsx
@@ -14,6 +14,7 @@ import UserManagement from "../components/admin_app/pages/UserManagement";
 import EditStudyContainer from "../components/admin_app/pages/EditStudyContainer";
 
 import "../scss/react-router-tabs.scss";
+import ensureCurrentUser from "./ensureCurrentUser";
 
 const NavBarLink = (props) => {
   let resolved = useResolvedPath(props.to);
@@ -26,84 +27,44 @@ const NavBarLink = (props) => {
   )
 }
 
-const AdminApp = (props) => {
+const AdminApp = () => {
   console.log('AdminApp')
-  if (props.tokenStatus !== 'user') {
-    return <Navigate to='/login'></Navigate>
-  }
 
-  if (!props.current_user.sync) {
-    if (!props.current_user.loading) {
-      if (!props.current_user.error) {
-        console.log(`reloading current user`);
-        window.setTimeout(props.loadCurrentUser, 50);
-      } else {
-        console.log(`No current user: ${JSON.stringify(props.current_user.error)}`);
-        // TODO: all 400 errors?
-        if (props.current_user.error.status === 401) {
-          return <Navigate to='/logout'></Navigate>
-        }
-      }
+  return ensureCurrentUser((currentUser) => {
+    if ((currentUser.data.role !== 'admin') && (currentUser.data.role !== 'investigator')) {
+      console.error('user is not an admin');
+      console.error(currentUser);
+      return <Navigate to='/participant'></Navigate>
     }
-    return <div>Loading...</div>
-  }
 
-  if ((props.current_user.data.role !== 'admin') && (props.current_user.data.role !== 'investigator')) {
-    console.error('user is not an admin');
-    console.error(props.current_user);
-    return <Navigate to='/participant'></Navigate>
-  }
+    return (
+      <div className="page-wrapper d-flex flex-column">
+        <HeaderContainer current_user={currentUser}></HeaderContainer>
+        <main id="wrap" className="admin-app-container app-container container flex-fill">
+          <div>
+            <nav className="navbar navbar-expand navbar-light">
+              <div>
+                <ul className="navbar-nav">
+                  <NavBarLink to="studies" title="Studies"></NavBarLink>
+                  <NavBarLink to="users" title="Users"></NavBarLink>
+                </ul>
+              </div>
+            </nav>
 
-  return (
-    <div className="page-wrapper d-flex flex-column">
-      <HeaderContainer current_user={props.current_user}></HeaderContainer>
-      <main id="wrap" className="admin-app-container app-container container flex-fill">
-        <div>
-          <nav className="navbar navbar-expand navbar-light">
-            <div>
-              <ul className="navbar-nav">
-                <NavBarLink to="studies" title="Studies"></NavBarLink>
-                <NavBarLink to="users" title="Users"></NavBarLink>
-              </ul>
-            </div>
-          </nav>
-
-          <Routes>
-            <Route exact path="/" element={<Navigate to="./studies" />} />
-            <Route exact path="/studies" element={<StudyManagement/>} />
-            <Route path="/users" element={<UserManagement/>} />
-            <Route path="/studies/:studyId/protocols/:protocolId"
-                   element={<ProtocolPreviewContainer/>}/>
-            <Route path="/studies/:studyId/edit"
-                   element={<EditStudyContainer/>}/>
-          </Routes>
-        </div>
-      </main>
-      <FooterContainer></FooterContainer>
-    </div>
-  )
+            <Routes>
+              <Route exact path="/" element={<Navigate to="./studies" />} />
+              <Route exact path="/studies" element={<StudyManagement/>} />
+              <Route path="/users" element={<UserManagement/>} />
+              <Route path="/studies/:studyId/protocols/:protocolId"
+                     element={<ProtocolPreviewContainer/>}/>
+              <Route path="/studies/:studyId/edit"
+                     element={<EditStudyContainer/>}/>
+            </Routes>
+          </div>
+        </main>
+        <FooterContainer></FooterContainer>
+      </div>
+    )
+  });
 }
-
-AdminApp.propTypes = {
-	current_user: CurrentUserType,
-	tokenStatus: PropTypes.string.isRequired,
-	userToken: UserTokenType,
-	loadStudies: PropTypes.func.isRequired,
-	loadCurrentUser: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-	current_user: state.current_user,
-	userToken: state.tokens.userToken,
-	tokenStatus: tokenStatus(state),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-	loadStudies: () => dispatch(elicitApi.actions.studies()),
-	loadCurrentUser: () => dispatch(elicitApi.actions.current_user()),
-});
-
-const connectedAdminApp = connect(mapStateToProps, mapDispatchToProps)(AdminApp);
-
-export default connectedAdminApp;
-export {connectedAdminApp as AdminApp};
+export default AdminApp;
--- app/javascript/apps/ParticipantApp.jsx
+++ app/javascript/apps/ParticipantApp.jsx
@@ -1,51 +1,23 @@
 //Import React and Dependencies
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {Navigate} from 'react-router-dom'
+import React from 'react';
 import ParticipantProtocolList from '../components/participant_app/components/ParticipantProtocolList'
 
 // Import Containers
 import HeaderContainer from "../components/nav/HeaderContainer";
 import FooterContainer from "../components/nav/FooterContainer";
 
-// Import Selectors
-import { tokenStatus } from '../reducers/selector';
+import ensureCurrentUser from "./ensureCurrentUser";
 
-// Import API
-import elicitApi from "../api/elicit-api.js";
+export const ParticipantApp = () => (
 
-export const ParticipantApp = () => {
-  const currentUser = useSelector(state => state.current_user);
-  const dispatch = useDispatch();
-
-  let currentTokenStatus = useSelector(state => tokenStatus(state));
-
-  useEffect(() => {
-    if (currentTokenStatus !== 'user') { return }
-
-    if (!currentUser) { return }
-
-    if (!currentUser?.sync && !currentUser?.loading) {
-      console.log("No current user!");
-      dispatch(elicitApi.actions.current_user());
-    }
-  }, [currentTokenStatus, currentUser, currentUser?.sync, currentUser?.loading]);
-
-  if (currentTokenStatus !== 'user') {
-    return <Navigate to='/login' />;
-  }
-
-  if (!currentUser.sync) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="page-wrapper d-flex flex-column">
-      <HeaderContainer current_user={currentUser} />
-      <main id="wrap" className="participant-app-container app-container container flex-fill">
-        <ParticipantProtocolList current_user={currentUser} />
-      </main>
-      <FooterContainer />
-    </div>
-  );
-};
+  ensureCurrentUser((currentUser) => (
+      <div className="page-wrapper d-flex flex-column">
+        <HeaderContainer current_user={currentUser} />
+        <main id="wrap" className="participant-app-container app-container container flex-fill">
+          <ParticipantProtocolList current_user={currentUser} />
+        </main>
+        <FooterContainer />
+      </div>
+    )
+  )
+);
